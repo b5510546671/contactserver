18ef6f27e4451fc1da5da17319836c810bf46013
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2014-09-15 01:16:53 +0700

    add data to Contact.java and ContactDao

--- src/contact/resource/Contact.java
+++ src/contact/resource/Contact.java
@@ -2,6 +2,11 @@ package contact.resource;
 
 import javax.persistence.Entity;
 
+/**
+ * 
+ * @author Supavit 5510546671
+ *
+ */
 @Entity
 public class Contact {
 	private int id;
--- src/contact/resource/ContactDao.java
+++ src/contact/resource/ContactDao.java
@@ -1,5 +1,33 @@
 package contact.resource;
 
-public class ContactDao {
+import java.util.List;
 
+import javax.ws.rs.*;
+
+/**
+ * 
+ * @author Supavit 5510546671
+ *
+ */
+public class ContactDao {
+	@GET
+	public Contact find(@PathParam("id") long id){
+		
+	}
+	@GET
+	public List<Contact> findAll(){
+		
+	}
+	@DELETE
+	public void delete(@PathParam("id") long id){
+		
+	}
+	@POST
+	public void save(Contact contact){
+		
+	}
+	@PUT
+	public void update(Contact contact){
+		
+	}
 }
