5388a627761301c300e4e07dfff60332e56aa5e9
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2014-09-15 01:23:32 +0700

    added annotations to ContactDao

--- src/contact/resource/ContactDao.java
+++ src/contact/resource/ContactDao.java
@@ -9,24 +9,30 @@ import javax.ws.rs.*;
  * @author Supavit 5510546671
  *
  */
+
 public class ContactDao {
 	@GET
+	@Path("/contacts/{id}")
 	public Contact find(@PathParam("id") long id){
 		
 	}
 	@GET
+	@Path("/contacts")
 	public List<Contact> findAll(){
 		
 	}
 	@DELETE
+	@Path("/contacts/{id}")
 	public void delete(@PathParam("id") long id){
 		
 	}
 	@POST
+	@Path("/contacts")
 	public void save(Contact contact){
 		
 	}
 	@PUT
+	@Path("/contacts/{id}")
 	public void update(Contact contact){
 		
 	}
